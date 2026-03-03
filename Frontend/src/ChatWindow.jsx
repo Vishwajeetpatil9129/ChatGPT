8637213970745b1a90cecf9ca4241d600a4d13e6
Vishwajeet Patil <vishwajeetpatil9129@gmail.com>
2026-03-04 02:35:02 +0530

    feat: implemented thread history and new chat functionality

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -6,11 +6,12 @@ import { ScaleLoader } from "react-spinners";
 import React, { useState , useEffect } from "react";
 
 function ChatWindow() {
-    const { prompt , setPrompt , reply , setReply , currThreadId , prevChats , setPrevChats} = useContext(MyContext);
+    const { prompt , setPrompt , reply , setReply , currThreadId , prevChats , setPrevChats , setNewChat} = useContext(MyContext);
     const [loading , setLoading] = useState(false);
 
     const getReply = async () => {
         setLoading(true);
+        setNewChat(false);
         console.log("message" , prompt , "threadId" , currThreadId);
         const options = {
             method: "post",
