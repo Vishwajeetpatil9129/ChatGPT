1210b65776e9ab05bb885b35dbb0b171203481fc
Vishwajeet Patil <vishwajeetpatil9129@gmail.com>
2026-03-13 00:37:49 +0530

    Add Clerk Publishable key Correctly

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { dark } from '@clerk/themes'  
+import { dark } from '@clerk/themes'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider 
-      publishableKey="pk_test_YmV0dGVyLXNhdHlyLTkwLmNsZXJrLmFjY291bnRzLmRldiQ"
-      appearance={{ baseTheme: dark }}  
+      publishableKey={PUBLISHABLE_KEY}
+      appearance={{ baseTheme: dark }}
     >
       <App />
     </ClerkProvider>
